fd496219f1a68c21f9ff54108f2dd11d3449b4c5
Thanmai10 <125534209+Thanmai10@users.noreply.github.com>
2023-02-19 12:51:46 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,11 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
+  },
+  "globals": {
+    "process": true
   }
 }
+
